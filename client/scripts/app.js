f863d9a591e7400abd5c43874a07cddda0464baf
Neil Vodoor <nvodoor@gmail.com>
2017-02-20 16:52:25 -0800

    Please commit this time.  Please enter the commit message for your changes. Lines starting

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,6 +6,8 @@ var Movie = Backbone.Model.extend({
 
   toggleLike: function() {
     // your code here
+    this.set('like', false)
+
   }
 
 });
@@ -16,12 +18,17 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    this.on('change', this.sort);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
+
+
+    this.comparator = field
+    this.sort();
   }
 
 });
@@ -59,6 +66,7 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.model.on('change', function () { this.render() }, this);
   },
 
   events: {
@@ -67,6 +75,7 @@ var MovieView = Backbone.View.extend({
 
   handleClick: function() {
     // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -80,6 +89,7 @@ var MoviesView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
